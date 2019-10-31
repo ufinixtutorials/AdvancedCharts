e6ce3cac183040511c6ee7dcfdd2e2acc4e77d42
elatech.limited <elatech.limited@outlook.com>
2019-10-31 08:31:36 +0100

    Add project files.

---
+++ MainActivity.cs
@@ -0,0 +1,202 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Microcharts.Droid;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using Microcharts;
+using SkiaSharp;
+using System.Globalization;
+using Android.Views;
+
+namespace AdvancedCharts
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        ChartView chartView;
+        TextView priceChartPriceTextView;
+        ImageView priceChartLogo;
+        private string btcPriceResponse;
+        private string ethPriceResponse;
+        private string eosPriceResponse;
+        private string steemPriceResponse;
+
+        LinearLayout priceHistoryLayout;
+        RelativeLayout priceProgressLayout;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            chartView = (ChartView)FindViewById(Resource.Id.chartView);
+            priceChartPriceTextView = (TextView)FindViewById(Resource.Id.priceChartPriceTextView);
+            priceChartPriceTextView.Click += PriceChartPriceTextView_Click;
+            priceChartLogo = (ImageView)FindViewById(Resource.Id.priceChartLogo);
+
+            priceHistoryLayout = (LinearLayout)FindViewById(Resource.Id.priceHistoryLayout);
+            priceProgressLayout = (RelativeLayout)FindViewById(Resource.Id.priceProgressLayout);
+
+            FetchChart();
+            FetchData();
+        }
+
+        private void PriceChartPriceTextView_Click(object sender, System.EventArgs e)
+        {
+            PopupMenu popupMenu = new PopupMenu(this, priceChartPriceTextView);
+            popupMenu.MenuItemClick += PopupMenu_MenuItemClick;
+
+            popupMenu.Menu.Add(Menu.None, 1, 1, "Bitcoin");
+            popupMenu.Menu.Add(Menu.None, 2, 2, "Ethereum");
+            popupMenu.Menu.Add(Menu.None, 3, 3, "EOS");
+            popupMenu.Menu.Add(Menu.None, 4, 4, "STEEM");
+
+            popupMenu.Show();
+        }
+
+        private void PopupMenu_MenuItemClick(object sender, PopupMenu.MenuItemClickEventArgs e)
+        {
+            if(e.Item.ItemId == 1)
+            {
+                RedrawChart(btcPriceResponse, "#f7931a");
+                priceChartLogo.SetImageResource(Resource.Drawable.btc_logo);
+            }
+            else if(e.Item.ItemId == 2)
+            {
+                RedrawChart(ethPriceResponse, "#0f0f0f");
+                priceChartLogo.SetImageResource(Resource.Drawable.eth);
+            }
+            else if (e.Item.ItemId == 3)
+            {
+                RedrawChart(eosPriceResponse, "#00a859");
+                priceChartLogo.SetImageResource(Resource.Drawable.eos);
+
+            }
+            else if (e.Item.ItemId == 4)
+            {
+                RedrawChart(steemPriceResponse, "#2a56c6");
+                priceChartLogo.SetImageResource(Resource.Drawable.steem);
+            }
+        }
+
+        async  void FetchChart()
+        {
+            string url = "https://api.roqqu.com/v1/history?symbol=btc";
+
+            var handler = new HttpClientHandler();
+            HttpClient client = new HttpClient(handler);
+            string jsonResponse = await client.GetStringAsync(url);
+
+            if (!string.IsNullOrEmpty(jsonResponse))
+            {
+                var resultObject = JObject.Parse(jsonResponse);
+                string success = resultObject["success"].ToString();
+
+                if(success != "true")
+                {
+                    return;
+                }
+
+                string dataString = resultObject["data"].ToString();
+                var chartData = JsonConvert.DeserializeObject<List<PriceData>>(dataString);
+
+                List<double> coinPrices = chartData.Select(x => x.price).ToList();
+                List<Entry> entries = new List<Entry>();
+
+                int takeData = 0;
+                foreach (double price in coinPrices)
+                {
+                    takeData++;
+
+                    Entry newentry = new Entry(float.Parse(price.ToString()))
+                    {
+                        Color = SKColor.Parse("#f7931a"),
+                    };
+
+                    if(takeData == 2)
+                    {
+                        entries.Add(newentry);
+                        takeData = 0;
+                    }
+                    
+                }
+
+                priceChartPriceTextView.Text = coinPrices[coinPrices.Count -1].ToString("$#,##0.00", CultureInfo.InvariantCulture);
+                double lowestPrices = coinPrices.Min();
+                var mainChart = new LineChart() { Entries = entries, LineMode = LineMode.Straight, LineSize = 2f, MinValue = float.Parse(lowestPrices.ToString()), PointMode = PointMode.None };
+                mainChart.Margin = 0;
+                chartView.Chart = mainChart;
+
+                priceProgressLayout.Visibility = ViewStates.Gone;
+                priceHistoryLayout.Visibility = ViewStates.Visible;
+            }
+        }
+
+        async void FetchData()
+        {
+            string btcurl = "https://api.roqqu.com/v1/history?symbol=btc";
+            string ethurl = "https://api.roqqu.com/v1/history?symbol=eth";
+            string eosurl = "https://api.roqqu.com/v1/history?symbol=eos";
+            string steemurl = "https://api.roqqu.com/v1/history?symbol=steem";
+
+            var handler = new HttpClientHandler();
+            HttpClient client = new HttpClient(handler);
+            btcPriceResponse = await client.GetStringAsync(btcurl);
+            ethPriceResponse = await client.GetStringAsync(ethurl);
+            eosPriceResponse = await client.GetStringAsync(eosurl);
+            steemPriceResponse = await client.GetStringAsync(steemurl);
+        }
+
+        void RedrawChart(string jsonResponse, string hexcolor)
+        {
+            if (!string.IsNullOrEmpty(jsonResponse))
+            {
+                var resultObject = JObject.Parse(jsonResponse);
+                string success = resultObject["success"].ToString();
+
+                if (success != "true")
+                {
+                    return;
+                }
+
+                string dataString = resultObject["data"].ToString();
+                var chartData = JsonConvert.DeserializeObject<List<PriceData>>(dataString);
+
+                List<double> coinPrices = chartData.Select(x => x.price).ToList();
+                List<Entry> entries = new List<Entry>();
+
+                int takeData = 0;
+                foreach (double price in coinPrices)
+                {
+                    takeData++;
+
+                    Entry newentry = new Entry(float.Parse(price.ToString()))
+                    {
+                        Color = SKColor.Parse(hexcolor),
+                    };
+
+                    if (takeData == 2)
+                    {
+                        entries.Add(newentry);
+                        takeData = 0;
+                    }
+
+                }
+
+                priceChartPriceTextView.Text = coinPrices[coinPrices.Count - 1].ToString("$#,##0.00", CultureInfo.InvariantCulture);
+                double lowestPrices = coinPrices.Min();
+                var mainChart = new LineChart() { Entries = entries, LineMode = LineMode.Straight, LineSize = 2f, MinValue = float.Parse(lowestPrices.ToString()), PointMode = PointMode.None };
+                mainChart.Margin = 0;
+                chartView.Chart = mainChart;
+
+            }
+        }
+    }
+}
---
+++ PriceData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace AdvancedCharts
+{
+   public class PriceData
+    {
+        public int id { get; set; }
+        public string token { get; set; }
+        public double price { get; set; }
+        public int _change { get; set; }
+        public string currency { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
+    }
+}
